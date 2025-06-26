204e4a63112dfcb9af1b06e620a6ac8bd6633e9e
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-06-26 14:58:52 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,10 @@ from color_detect import detect_dominant_color_object
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = 'outputs'
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
